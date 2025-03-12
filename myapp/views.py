15a2e8e5708d2aa1b56244312061dbffdbe63d90
far <farhan.shakeel@icloud.com>
2025-03-12 17:36:16 +0000

    fix: remove unnecessary redirect import in views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
